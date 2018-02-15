67a56837a7148ba9e66f5c51ad6126b3f6b0fc26
Marcus Richmond <marcu5@countermail.com>
2018-02-15 11:55:49 -0500

    returning one result to DOM using first random number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let STORE = {
   randomNumbers: [],
+  venueType: ["food", "drinks"],
 }
 
 //Get first and second random numbers and push into STORE
@@ -14,18 +15,40 @@ function fetchRandomNumbers(){
   });
 };
 
-function appendToDom(selector, content){
-  $(selector).append(content);
+//take returned JSON and create HTML section for each venue
+
+
+function generateResultHTML(returnedDataObject){
+  let photoPrefix = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.featuredPhotos.items[0].prefix;
+  let photoSuffix = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.featuredPhotos.items[0].suffix;
+  let photo = `${photoPrefix}300x300${photoSuffix}`;
+  let venueName = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.name;
+  let firstLineAddress = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.location.address;
+  let city = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.location.city;
+  let postCode = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.location.postalCode;
+  let phoneNumber = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.contact.formattedPhone;
+  let faceBookCode = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.contact.facebook;
+  let facebookURL = `https://facebook.com/${faceBookCode}`
+  let website = returnedDataObject.response.groups[0].items[STORE.randomNumbers[0]].venue.url
+
+  return `
+  <div class="venueContainer">
+    <img src=${photo}>
+    <h2>${venueName}</h2>
+    <div>${firstLineAddress}</div>
+    <div>${city}</div>
+    <div>${postCode}</div>
+    <div>${phoneNumber}</div>
+    <a href="${facebookURL}">Facebook</a>
+    <a href="${website}">Website</a>
+  </div>
+  `
 }
 
-function generateResultHTML(data){
-  let photoPrefix = data.response.groups[0].items[0].venue.featuredPhotos.items[0].prefix;
-  let photoSuffix = data.response.groups[0].items[0].venue.featuredPhotos.items[0].suffix;
 
-  return `<img src="${photoPrefix}300x300${photoSuffix}" alt="bar photo">`;
-}
+//formats and sends query to Foursquare API, then pushes to DOM
 
-function queryFourSqAPI (location, priceLevel){
+function queryFourSqAPI (location, priceLevel, section){
   var clientId = 'K0D0DBU3DHLWKXDSCSC0L5PK1RKTWD2LLKDYWCWYCE22XI55';
   var clientSecret = 'QZAEIAQCIA01JOQBW5QGYG01N2LNPYN2P0PRL3DDUGZOWAIK';
   var url = "https://api.foursquare.com/v2/venues/explore";
@@ -37,7 +60,7 @@ function queryFourSqAPI (location, priceLevel){
       client_secret: clientSecret,
       near: location,
       radius: 5000,
-      section: "food",
+      section: section,
       limit: 10,
       time: "any",
       day: "any",
@@ -46,73 +69,32 @@ function queryFourSqAPI (location, priceLevel){
       v: Date.now(),
     }
   }
+
+
+
   $.ajax(settings).done(data=>{
-    const result = generateResultHTML(data);
+    console.log(data);
+    const result = generateResultHTML(data, STORE.randomNumbers[0]);
     appendToDom('.js-results', result);
   })
 }
 
+
+//appends formated html to a selected DOM element
+function appendToDom(selector, content){
+  $(selector).append(content);
+}
+
+//initialises the app's code
 function setupApp(){
   fetchRandomNumbers();
   $('.js-search-form').submit(event => {
     event.preventDefault();
     let location = $('input').val();
     let priceLevel = $('input[name=richPoor]:checked').val();
-    queryFourSqAPI(location, priceLevel);
+    queryFourSqAPI(location, priceLevel, "food");
   });
 };
 
+//actions the initialisation
 $(setupApp);
-
-// get location and price level and send request for 10 bars
-// $('.js-search-form').submit(event => {
-//   event.preventDefault();
-//   let location = $('input').val();
-//   let priceLevel = $('input[name=richPoor]:checked').val();
-//   console.log(location);
-//   console.log(priceLevel);
-//
-//   var clientId = 'K0D0DBU3DHLWKXDSCSC0L5PK1RKTWD2LLKDYWCWYCE22XI55';
-//   var clientSecret = 'QZAEIAQCIA01JOQBW5QGYG01N2LNPYN2P0PRL3DDUGZOWAIK';
-//   var url = "https://api.foursquare.com/v2/venues/explore";
-//   var settings = {
-//     method: "GET",
-//     url: url,
-//     data: {
-//       client_id: clientId,
-//       client_secret: clientSecret,
-//       near: location,
-//       radius: 5000,
-//       section: "drinks",
-//       limit: 10,
-//       price: priceLevel,
-//       v: Date.now(),
-//       time: "any",
-//       day: "any",
-//       venuePhotos: true,
-//     }
-//   }
-//   $.ajax(settings).done(data=>{
-//     console.log("bars", data);
-//   })
-// })
-//
-// function createVenueElement(object, index){
-//   let photoPrefix = object.response.groups["0"].items["index"].venue.featuredPhotos.items["0"].prefix;
-//   let photoSuffix = object.response.groups["0"].items["index"].venue.featuredPhotos.items["0"].suffix;
-//   let photoString = `<img src="${photoPrefix}300x300${photoSuffix}" alt="bar photo">`;
-//
-//
-//   return `
-//     <div>
-//       <img src="${photoPrefix}300x300${photoSuffix}" alt="bar photo">
-//       <h2>
-//     </div>
-//   `
-//   $('.js-results').append(photoString);
-// }
-
-
-
-
-//render map with both locations
