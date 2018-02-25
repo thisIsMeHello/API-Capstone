a1d1195a4da44be3951856cec49653a02f7d6b8a
Marcus Richmond <marcu5@countermail.com>
2018-02-25 15:21:17 -0500

    finished a11y, need to final check

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let STORE = {
   venueType: ["food", "drinks"],
   domLocations: [$(".js-resultsFood"), $(".js-resultsDrinks")],
   activity: ["eat", "drink"],
-  errorMsg: ["No food results, try again or change your search options!", "No bar results, try again or change your search options!"]
+  errorMsg: ["No food results with that location and price, try again or change your search options!", "No bar results at that location and price, try again or change your search options!"]
 }
 
 //Get first and second random numbers and push into STORE
@@ -15,46 +15,40 @@ function fetchRandomNumbers(){
     let singleRandomDigitOne = parseInt(returnedRandomOne.toString().split('').pop());
     let singleRandomDigitTwo = parseInt(returnedRandomTwo.toString().split('').pop());
     STORE.randomNumbers.push(singleRandomDigitOne, singleRandomDigitTwo);
-    console.log("Quantum Random Numbers", STORE.randomNumbers)
   });
 };
 
+//backup number generation if quantum numbers not available
 function generateRandomNumbers(){
   for (let i=0; i<2; i++){
     let randomNum = Math.floor((Math.random() * 5) + 1);
     STORE.localRandomNumbers.push(randomNum);
-    console.log("locally generated", STORE.localRandomNumbers);
   };
 };
 
 //take returned JSON and create HTML section for each venue
-
-
 function generateResultHTML(data, array, i){
   try {
-    let venue = data.response.groups[0].items[array[i]].venue;
-    let activity = STORE.activity[i];
-    let photoPrefix = venue.featuredPhotos.items[0].prefix;
-    let photoSuffix = venue.featuredPhotos.items[0].suffix;
-    let photo = `${photoPrefix}300x300${photoSuffix}`;
-    let venueName = venue.name;
-    let rating = `rating ${venue.rating}`;
-    let firstLineAddress = venue.location.address;
-    let city = venue.location.city;
-    let postCode = venue.location.postalCode;
-    let phoneNumber = venue.contact.formattedPhone;
-    let faceBookCode = venue.contact.facebook;
-    let facebookURL = `https://facebook.com/${faceBookCode}`;
-    let website = venue.url;
+    const venue = data.response.groups[0].items[array[i]].venue;
+    const activity = STORE.activity[i];
+    const photoPrefix = venue.featuredPhotos.items[0].prefix;
+    const photoSuffix = venue.featuredPhotos.items[0].suffix;
+    const photo = `${photoPrefix}300x300${photoSuffix}`;
+    const venueName = venue.name;
+    const rating = `rating ${venue.rating}`;
+    const firstLineAddress = venue.location.address;
+    const city = venue.location.city;
+    const postCode = venue.location.postalCode;
+    const phoneNumber = venue.contact.formattedPhone;
+    const faceBookCode = venue.contact.facebook;
+    const facebookURL = `https://facebook.com/${faceBookCode}`;
+    const website = venue.url;
 
     return `
     <section class="venueContainer inlineBlock">
-
       <p class="eatDrink montAltFont">${activity}</p>
-
       <div style="display: flex; align-items: flex-end; flex-wrap: wrap">
-
-        <img class="inlineBlock" src=${photo}>
+        <img class="inlineBlock" src=${photo} alt="Venue photo">
         <address>
           <div class="inlineBlock">
             <h2 class="venueName">${venueName}</h2>
@@ -62,15 +56,14 @@ function generateResultHTML(data, array, i){
               <a href="${facebookURL}"><i class="fab fa-facebook-square colorWhite icon"></i></a><a href="${website}"><i class="fas fa-external-link-square-alt colorWhite icon"></i></a><span class="">${rating}</span>
             </div>
           </div>
-
           <p class="firstLineAddress">${firstLineAddress}</p>
           <p>${city}</p>
           <p>${postCode}</p>
-          <p class="phoneNumber inlineBlock"><i class="fas fa-phone inlineBlock"></i> ${phoneNumber === undefined ? "not available" : phoneNumber}</p>
+          <p class="phoneNumber inlineBlock">
+            <i class="fas fa-phone inlineBlock"></i> ${phoneNumber === undefined ? "not available" : phoneNumber}
+          </p>
         </address>
-
       </div>
-
     </section>
 `
 
@@ -80,19 +73,15 @@ function generateResultHTML(data, array, i){
     <p>${STORE.errorMsg[i]}</p>
     `
   }
-}
+};
 
 
 //formats and sends query to Foursquare API, then pushes to DOM
-//get run twice by setupApp using following data:
-// - location, priceLevel, food, 0
-// - location, priceLevel, drinks, 1
-
 function queryFourSqAPI (location, priceLevel, section, i){
-  var clientId = 'K0D0DBU3DHLWKXDSCSC0L5PK1RKTWD2LLKDYWCWYCE22XI55';
-  var clientSecret = 'QZAEIAQCIA01JOQBW5QGYG01N2LNPYN2P0PRL3DDUGZOWAIK';
-  var url = "https://api.foursquare.com/v2/venues/explore";
-  var settings = {
+  const clientId = 'K0D0DBU3DHLWKXDSCSC0L5PK1RKTWD2LLKDYWCWYCE22XI55';
+  const clientSecret = 'QZAEIAQCIA01JOQBW5QGYG01N2LNPYN2P0PRL3DDUGZOWAIK';
+  const url = "https://api.foursquare.com/v2/venues/explore";
+  const settings = {
     method: "GET",
     url: url,
     data: {
@@ -108,42 +97,25 @@ function queryFourSqAPI (location, priceLevel, section, i){
       price: priceLevel,
       v: Date.now(),
     }
-  }
+  };
 
-  $.ajax(settings).done(data=>{
-    console.log(data);
-    $('input').val("");
+  $.ajax(settings).done(data => {
+    // $('input').val("");
     $('.resultsPage').removeClass("hidden");
-    $('.homePage').addClass("hidden");
-    $('.js-video').addClass("hidden");
+    $('.homePageContainer').hide();
     let array = randomArrayChooser();
-    // let randomNum = array[i];
     const result = generateResultHTML(data, array, i);
     appendToDom(STORE.domLocations[i], result);
   }).fail(response=>{
     $('.location-input-error').show();
     $('input').val("");
-  })
+  });
 }
 
-// function randomArrayChooser() {
-//   if (typeof STORE.randomNumbers[0] !== "undefined"){
-//     console.log("used STORE.randomNumbers")
-//     return STORE.randomNumbers;
-//   } else {
-//     console.log("used STORE.localRandomNumbers")
-//     return STORE.localRandomNumbers;
-//   };
-// }
-
-
 function randomArrayChooser(){
-  console.log(STORE.randomNumbers);
   if (STORE.randomNumbers.length > 0){
-    console.log("used STORE.randomNumbers")
     return STORE.randomNumbers;
   } else {
-    console.log("used STORE.localRandomNumbers")
     return STORE.localRandomNumbers;
   }
 }
@@ -157,6 +129,7 @@ function appendToDom(domSelector, content){
 function setupApp(){
   fetchRandomNumbers();
   generateRandomNumbers();
+
   $('.js-search-form').submit(event => {
     event.preventDefault();
     let location = $('input').val();
@@ -165,17 +138,12 @@ function setupApp(){
         let section = STORE.venueType[i];
         queryFourSqAPI(location, priceLevel, section, i);
       }
-
-    tryAgain();
   });
-};
 
-//resets app to home page
-function tryAgain(){
   $('.tryAgain').on("click", function(){
     window.location.reload();
   })
-}
+};
 
 //actions the initialisation
 $(setupApp);
